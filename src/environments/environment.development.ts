0e97bb556befa69a87d2d67f16d7504dc03aaed9
joao-rafael <joaorafa131@gmail.com>
2023-12-30 17:30:54 -0300

    chore: update gitignore

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    mapsApi: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCm4JsjNY5OZ1wMznd_yDSK7UDMLfTjFUE&callback=console.debug&libraries=maps,marker&v=beta',
-    // mobi7Api: 'https://challenge-backend.mobi7.io' real api
-    mobi7Api: 'http://localhost:3000' // mock api using mock-server
-};
